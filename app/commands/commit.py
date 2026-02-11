e06ea8b22249721082240530486dad68ec66dc9f
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2026-02-11 20:05:52 +0100

    feat: commit

--- app/commands/commit.py
+++ app/commands/commit.py
@@ -1,73 +1,72 @@
 from argparse import _SubParsersAction
-import os
-from typing import List, Tuple
+from datetime import datetime, timezone
+from typing import Optional
 
+from app.cli import logger
+from app.objects import GitCommit
+from app.repository import GitIndex, GitRepository
+from app.repository.branch import update_ref
+from app.repository.commit import tree_from_index
+from app.repository.config import get_user_from_config
 
-from .rm import rm
 from .command import cmd
-from app.objects import GitBlob
-from app.repository import GitRepository, GitIndex, GitIndexEntry, GitObjects
 
 
 def setup_parser(subparsers: _SubParsersAction) -> None:
     parser = subparsers.add_parser("commit", help="Record changes to the repository")
     parser.add_argument("-m", metavar="message", dest="message", help="Commit message")
-    parser.set_defaults(func=cmd_add)
+    parser.set_defaults(func=cmd_commit)
 
 
 @cmd(req_repo=True)
-def cmd_add(args, repo: GitRepository) -> None:
-    add(repo, args.path)
-
-
-def add(repo: GitRepository, paths: List[str], delete=False, skip_missing=False):
-    rm(repo, paths, delete=False, skip_missing=True)
-
-    worktree_prefix = repo.worktree + os.sep
-
-    pair_paths: Tuple[str, str] = set()
-
-    for path in paths:
-        abspath = os.path.abspath(path)
-        if not abspath.startswith(worktree_prefix):
-            raise Exception(f"Path is outside of worktree: {abspath}")
-        if not os.path.isfile(abspath):
-            raise Exception(f"Not a file: {abspath}")
-        relpath = os.path.relpath(abspath, repo.worktree)
-        pair_paths.add((abspath, relpath))
-
+def cmd_commit(args, repo: GitRepository) -> None:
     index = GitIndex.read(repo)
 
-    for abspath, relpath in pair_paths:
-        with open(abspath, "rb") as file:
-            data = file.read()
+    if not index.entries:
+        logger.info("Nothing to commit (create/copy files and use 'mgit add')")
+        return
 
-            obj = GitBlob.deserialize(data)
-            raw = GitObjects.raw(obj)
-            sha = GitObjects.hash(raw)
+    tree_sha = tree_from_index(repo, index)
 
-            stat = os.stat(abspath)
+    try:
+        parent_sha = repo.objects.find("HEAD")
+    except FileNotFoundError:
+        parent_sha = None
 
-            ctime_s = int(stat.st_ctime)
-            ctime_ns = stat.st_ctime_ns % 10**9
-            mtime_s = int(stat.st_mtime)
-            mtime_ns = stat.st_mtime_ns % 10**9
+    author = get_user_from_config(repo.config)
+    commit_sha = create_commit(
+        repo,
+        tree_sha,
+        parent_sha,
+        author,
+        args.message,
+    )
 
-            entry = GitIndexEntry(
-                ctime=(ctime_s, ctime_ns),
-                mtime=(mtime_s, mtime_ns),
-                dev=stat.st_dev,
-                ino=stat.st_ino,
-                mode_type=0b1000,
-                mode_perms=0o644,
-                uid=stat.st_uid,
-                gid=stat.st_gid,
-                fsize=stat.st_size,
-                sha=sha,
-                assume_valid=False,
-                stage=False,
-                name=relpath,
-            )
-            index.entries.append(entry)
+    update_ref(repo, commit_sha)
+    logger.info(f"[{commit_sha[:7]}] {args.message.splitlines()[0]}")
 
-    index.write(repo)
+
+def create_commit(
+    repo: GitRepository,
+    tree_sha: str,
+    parent_sha: Optional[str],
+    author: str,
+    message: str,
+) -> str:
+    commit = GitCommit()
+    commit.kvlm.set(b"tree", tree_sha.encode("ascii"))
+
+    if parent_sha:
+        commit.kvlm.set(b"parent", parent_sha.encode("ascii"))
+
+    now = datetime.now(timezone.utc).astimezone()
+    timestamp = now.strftime("%s %z")
+
+    author_data_bytes = f"{author} {timestamp}".encode("utf-8")
+    commit.kvlm.set(b"author", author_data_bytes)
+    commit.kvlm.set(b"committer", author_data_bytes)
+    commit.kvlm.set_message(message.strip().encode("utf-8") + b"\n")
+
+    sha = repo.objects.write(commit)
+
+    return sha
